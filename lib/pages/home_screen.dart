41e96e520e67eb0ee1169fba412a6b8baeb8a06b
iqbal-hossain-01 <iqbalhossain207513@gmail.com>
2024-10-08 22:57:22 +0600

    AppBar with Gradient

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -5,62 +5,25 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text("Categories"),
-          actions: [
-            IconButton(
-              onPressed: () {
-                // Search Action
-              },
-              icon: const Icon(Icons.search),
-            ),
-            IconButton(
-              onPressed: () {
-                // Vert Action
-              },
-              icon: const Icon(Icons.more_vert),
-            ),
-          ],
-          bottom: const TabBar(
-            tabs: [
-              Tab(
-                text: "Home",
-              ),
-              Tab(
-                text: "Trending",
-              ),
-              Tab(
-                text: "Favorites",
-              ),
-            ],
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Fashion",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 24.0,
+            color: Colors.white,
           ),
-          backgroundColor: Colors.redAccent,
         ),
-        drawer: const Drawer(),
-        body: TabBarView(
-          children: [
-            const Center(
-              child: Text(
-                "Home Content",
-              ),
-            ),
-            const Center(
-              child: Text(
-                "Trending Content",
-              ),
-            ),
-          
-            Scaffold(
-                floatingActionButton: FloatingActionButton(
-                  onPressed: () {},
-                  child: const Icon(Icons.add),
-                ),
-              ),
-          ],
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+            colors: [Colors.pink, Colors.orange],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          )),
         ),
+        elevation: 0,
       ),
     );
   }
